0b84f51ce81ca770794483e38f3e8a26466b57dd
g.balayan <g.balayan@notamedia.ru>
2023-10-14 19:34:22 +0300

    Initial Commit

---
+++ gurgenapp/gurgenappApp.swift
@@ -0,0 +1,17 @@
+//
+//  gurgenappApp.swift
+//  gurgenapp
+//
+//  Created by notamedia on 14.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct gurgenappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
